32cf99d895481a447f4f7c7d126155d0b3b7b5b0
Azamat Alimbayev <azakost@gmail.com>
2021-11-08 21:51:56 +0600

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+const https = require('https');
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', (qr) => {
+    https.get('https://qwix.kz/api/wa/send-qr?data=' + encodeURIComponent(qr));
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    var phone = msg.from.substr(1, 10);
+    console.log(phone);
+    https.get('https://qwix.kz/api/wa/get-code?phone=' + phone, (res) => {
+        res.on('data', (code) => {
+            msg.reply(code.toString());
+        });
+    });
+});
+
+client.on('change_state', (e) => {
+    https.get('https://qwix.kz/api/wa/report?msg=' + encodeURIComponent(e));
+});
+
+client.initialize();
+
+
